86439d07c57872edd438443f89bed00058bc515d
moisessmello <moisesmelo70@gmail.com>
2024-08-15 21:59:05 -0300

    aula 15/08

---
+++ fundamentos1/src/app/fundamentos/page.js
@@ -0,0 +1,16 @@
+import Cabecalho from "../components/Cabecalho";
+
+export default function Fundamentos() {
+
+    
+        
+    return (
+        
+        <>
+            <Cabecalho />
+            <h1>Fundamentos</h1>
+            <p>Sucesso</p>
+            <Cabecalho />
+        </>
+    )
+}
